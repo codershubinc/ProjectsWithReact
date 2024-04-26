2c0ae95e4dfb21685d9b176914f69fedfef20ba0
Swapnil ingle <90494823+CodersHub-in@users.noreply.github.com>
2024-04-26 23:18:13 +0530

    login logout and conf , config file create

---
+++ BlogWebWithBackend/src/appwrite/auth.js
@@ -0,0 +1,60 @@
+import conf from '../conf/conf'
+import { Client, Account, ID } from "appwrite";
+
+export class AuthService {
+    Client = new Client();
+    account;
+
+    constructor() {
+        this.Client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.Client);
+    }
+    async createAccount(email, password, name) {
+        try {
+
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                // call another method
+                return this.login(email, password)
+
+            } else {
+                return userAccount;
+            }
+
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async login(email, password) {
+        try {
+            return await this.account.createEmailSession(email, password);
+
+        } catch (error) {
+
+            throw error;
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            throw error;
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+const authService = new AuthService();
+
+export default authService;
